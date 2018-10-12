1959a79a0c8f58f1eaf6e2220c223781460d7365
ahza64 <ahza64@yahoo.com>
2018-10-11 17:23:21 -0700

    init

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(express.static('build'));
 
 app.get(['/'], function (req, res) {
-  res.sendFile(__dirname + '/build');
+  res.sendFile(__dirname + '/build/index.html');
 });
 
 app.get(['/*'], function (req, res) {
