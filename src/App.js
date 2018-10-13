4e06e1953670732c301600c3f414dc1daa730560
ahza64 <ahza64@yahoo.com>
2018-10-12 20:23:50 -0700

    init

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import logo from './PRSC110.png';
 import './App.css';
 
 class App extends Component {
-  // @TODO serviceWroker is stopping app updates. "Bypass for network" will yield you the updated package, and sw seems to store the updated package
-  // I need serviceWorker to update app when network files change
+  // @TODO (no bugs in production) serviceWroker is stopping app updates. "Bypass for network" will yield you the updated package, and sw seems to store the updated package
+  // I need serviceWorker to update app when network files change (only does it in development)
+  // no bug in production, update protfolio and resume, etc to remove sensative info, to more widley spread my info
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi Mom. Hi Dad. Just spinnin' in ma boat!
+            Hi Mom. Hi Dad. Just spinnin' in ma boat!!
           </p>
           <a
             className="App-link"
